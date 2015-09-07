f9f1c812ce57caf269a228e816f664e86e3e1c98
Christopher Wood <christopherwood07@gmail.com>
2015-09-07 11:21:20 -0700

    name and message decoding

--- iris/src/packet/decoder.rs
+++ iris/src/packet/decoder.rs
@@ -12,11 +12,11 @@ pub fn decode_packet_intro(slice: &[u8], mut offset: usize) {
 
     // Debug header info
     println!("TLV = {} {} {}", version, msg_type, plength);
+    println!("      {}", rsvd);
     if msg_type == 0 {
-        println!("interest time...");
-        decode_tlv_interest(plength, &slice[offset .. (offset + plength as usize)]);
+        decode_tlv_interest(slice, plength, offset);
     } else if msg_type == 1 {
-        decode_tlv_content_object(plength, &slice[offset .. (offset + plength as usize)]);
+        decode_tlv_content_object(slice, plength, offset);
     }
 }
 
@@ -28,40 +28,73 @@ fn decode_tlv_parse_two(slice: &[u8], offset: usize) -> (u16) {
     return ((slice[offset] as u16) << 8) | (slice[offset + 1] as u16);
 }
 
-fn decode_tlv_interest(plength: u16, slice: &[u8]) {
-    // TODO
+fn decode_tlv_interest(slice: &[u8], plength: u16, mut offset: usize) {
     println!("decoding an interest");
 }
 
-fn decode_tlv_content_object(plength: u16, slice: &[u8]) {
-    // TODO
+fn decode_tlv_content_object(slice: &[u8], plength: u16, mut offset: usize) {
     println!("decoding a content object");
-    let mut consumed: usize = decode_tlv_message(plength, slice);
+    let mut consumed: usize = decode_tlv_message(slice, plength, offset);
 }
 
-fn decode_tlv_message(plength: u16, slice: &[u8]) -> (usize) {
-    let mut offset: usize = 0;
-    let message_type: u16 = decode_tlv_parse_two(slice, offset); offset += 2;
-    let message_length: u16 = decode_tlv_parse_two(slice, offset); offset += 2;
+fn decode_tlv_message(slice: &[u8], plength: u16, mut offset: usize) -> (usize) {
+    let top_type: u16 = decode_tlv_parse_two(slice, offset); offset += 2;
+    let top_length: u16 = decode_tlv_parse_two(slice, offset); offset += 2;
 
-    println!("message TL = {} {}", message_type, message_length);
-
-    let mut consumed: usize = offset; // number of bytes consumed
+    println!("top level TL = {} {}", top_type, top_length);
 
     // TODO: replace these magic numbers with constants
-    if message_type == 0 {
+    // TODO: do something different for each type?
+    if top_type == 1 {
 
-    } else if message_type == 1 {
-        decode_tlv_payload(message_length, &slice[consumed .. (consumed + message_length as usize)]);
+    } else if top_type == 2 {
+
     }
 
-    return consumed;
+    // The name is mandatory
+    let mut next_type: u16 = decode_tlv_parse_two(slice, offset); offset += 2;
+    let mut next_length: u16 = decode_tlv_parse_two(slice, offset); offset += 2;
+    println!("TL = {} {}", next_type, next_length);
+    if next_type == 0 {
+        offset = decode_tlv_name_value(slice, next_length, offset);
+    }
+
+    // Check what's next
+    next_type = decode_tlv_parse_two(slice, offset); offset += 2;
+    next_length = decode_tlv_parse_two(slice, offset); offset += 2;
+    println!("TL = {} {}", next_type, next_length);
+    if next_type == 1 {
+        offset = decode_tlv_payload_value(slice, next_length, offset);
+    }
+
+    return offset;
 }
 
-fn decode_tlv_payload(plength: u16, slice: &[u8]) -> (usize) {
-    return 0;
+fn decode_tlv_payload_value(slice: &[u8], plength: u16,  mut offset: usize) -> (usize) {
+    let payload_value: &[u8] = &slice[offset .. (offset + plength as usize)];
+
+    for b in payload_value {
+        print!("{:X} ", b);
+    }
+    println!("");
+
+    return offset;
 }
 
-fn decode_tlv_name(plength: u16, slice: &[u8]) -> (usize) {
-    return 0;
+fn decode_tlv_name_value(slice: &[u8], plength: u16,  mut offset: usize) -> (usize) {
+    let target: usize = (plength as usize) + offset;
+    while offset < target {
+        let name_segment_type: u16 = decode_tlv_parse_two(slice, offset); offset += 2;
+        let name_segment_length: u16 = decode_tlv_parse_two(slice, offset); offset += 2;
+        let name_segment_value: &[u8] = &slice[offset .. (offset + name_segment_length as usize)];
+        offset += name_segment_length as usize;
+
+        print!("NAME TLV {} {} ", name_segment_type, name_segment_length);
+        for b in name_segment_value {
+            print!("{:X} ", b);
+        }
+        println!("");
+    }
+
+    return offset;
 }
