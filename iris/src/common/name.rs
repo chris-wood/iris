797927202b2c94f05d8f9434dda4c44a060c7799
Christopher Wood <christopherwood07@gmail.com>
2015-10-07 10:10:06 -0700

    Name constructor stub

--- iris/src/common/name.rs
+++ iris/src/common/name.rs
@@ -4,3 +4,11 @@ use std::string::String as String;
 pub struct Name {
     components: Vec<String>
 }
+
+impl Name {
+    pub fn new(components: Vec<String>) -> Name {
+        Name {
+            components: components
+        }
+    }
+}
