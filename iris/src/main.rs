250f2ca91dcc19209cdf0cdc3c47bd75fe2c7b97
Christopher Wood <christopherwood07@gmail.com>
2015-10-08 10:35:08 -0700

    Main update -- focus on processor construction

--- iris/src/main.rs
+++ iris/src/main.rs
@@ -35,49 +35,49 @@ fn main() {
 
     let buffer = &file_contents[..]; // take reference to the entire thing (i.e., a slice)
 
-    let msg = core::packet::decode_packet(buffer);
-    msg.print();
-
     // Create the forwarder and message processor
     let fwd = core::Forwarder::new();
     let (tx, rx): (Sender<core::packet::message::Message>, Receiver<core::packet::message::Message>) = mpsc::channel();
     let processor = core::processor::Processor::new(fwd, rx);
 
-    let mut listeners = Vec::new();
+    let msg = core::packet::decode_packet(buffer);
+    processor.process_message(msg);
 
-    // Create listeners for all faces
-    let localhost = Ipv4Addr::new(127,0,0,1);
-    let defaultUdpAddr = std::net::SocketAddrV4::new(localhost, 9696);
-    let defaultUdpListener = core::link::UDPLinkListener::new(defaultUdpAddr);
-    let listenResult = defaultUdpListener.listen();
-    match listenResult {
-        Ok(listener) => {
-            println!("Created the UDP listener");
-            listeners.push(listener);
-        },
-        Err(e) => {
-            println!("Could not listen on UDP path");
-        }
-    }
-
-    let defaultTcpAddr = std::net::SocketAddrV4::new(localhost, 9697);
-    let defaultTcpListener = core::link::TCPLinkListener::new(defaultTcpAddr);
-    let listenResult = defaultTcpListener.listen();
-    match listenResult {
-        Ok(listener) => {
-            println!("Created the TCP listener");
-            listeners.push(listener);
-        },
-        Err(e) => {
-            println!("Could not listen on TCP path");
-        }
-    }
-
-    // Open the command REPL
-    control::control_repl();
-
-    println!("Joining on the listeners");
-    for listener in listeners {
-        listener.join();
-    }
+    // let mut listeners = Vec::new();
+    //
+    // // Create listeners for all faces
+    // let localhost = Ipv4Addr::new(127,0,0,1);
+    // let defaultUdpAddr = std::net::SocketAddrV4::new(localhost, 9696);
+    // let defaultUdpListener = core::link::UDPLinkListener::new(defaultUdpAddr);
+    // let listenResult = defaultUdpListener.listen();
+    // match listenResult {
+    //     Ok(listener) => {
+    //         println!("Created the UDP listener");
+    //         listeners.push(listener);
+    //     },
+    //     Err(e) => {
+    //         println!("Could not listen on UDP path");
+    //     }
+    // }
+    //
+    // let defaultTcpAddr = std::net::SocketAddrV4::new(localhost, 9697);
+    // let defaultTcpListener = core::link::TCPLinkListener::new(defaultTcpAddr);
+    // let listenResult = defaultTcpListener.listen();
+    // match listenResult {
+    //     Ok(listener) => {
+    //         println!("Created the TCP listener");
+    //         listeners.push(listener);
+    //     },
+    //     Err(e) => {
+    //         println!("Could not listen on TCP path");
+    //     }
+    // }
+    //
+    // // Open the command REPL
+    // control::control_repl();
+    //
+    // println!("Joining on the listeners");
+    // for listener in listeners {
+    //     listener.join();
+    // }
 }
