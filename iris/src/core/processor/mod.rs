5fcee6595d2595e737dddd0428822ad1ce13b6d1
Christopher Wood <christopherwood07@gmail.com>
2015-12-19 09:11:42 -0800

    Switching to async IO with mio

--- iris/src/core/processor/mod.rs
+++ iris/src/core/processor/mod.rs
@@ -16,51 +16,56 @@ use std::thread;
 
 pub struct Processor<'a> {
     fwd: Forwarder<'a>,
-    input_queue: Receiver<(Message, u16)>,
-    control_channel: Receiver<String>,
-    output_queue: Sender<(Message, u16)>,
-    link_manager: LinkManager
+    // input_queue: Receiver<(Message, u16)>,
+    // control_channel: Receiver<String>,
+    // output_queue: Sender<(Message, u16)>,
+    // link_manager: LinkManager
+}
+
+pub enum ProcessorError {
+    UnsupportedMessageType
 }
 
 impl<'a> Processor<'a> {
-    pub fn new(fwdRef: Forwarder<'a>, input_channel: Receiver<(Message, u16)>, output_channel: Sender<(Message, u16)>, control: Receiver<String>, link_manager: LinkManager) -> Processor {
+    pub fn new(fwdRef: Forwarder<'a>) -> Processor { // }, input_channel: Receiver<(Message, u16)>, output_channel: Sender<(Message, u16)>, control: Receiver<String>, link_manager: LinkManager) -> Processor {
         Processor {
             fwd: fwdRef,
-            input_queue: input_channel,
-            control_channel: control,
-            output_queue: output_channel,
-            link_manager: link_manager
+            // input_queue: input_channel,
+            // control_channel: control,
+            // output_queue: output_channel,
+            // link_manager: link_manager
         }
     }
 
     pub fn run(&mut self) {
         loop {
-            let traffic_attempt = self.input_queue.try_recv();
-            match traffic_attempt {
-                Ok((msg, face)) => {
-                    println!("Processing a message!");
-                    self.process_message(msg, face);
-                },
-                Err(e) => {
-                    // println!("Error receiving a message");
-                }
-            }
+            // let traffic_attempt = self.input_queue.try_recv();
+            // match traffic_attempt {
+            //     Ok((msg, face)) => {
+            //         println!("Processing a message!");
+            //         self.process_message(msg, face);
+            //     },
+            //     Err(e) => {
+            //         // println!("Error receiving a message");
+            //     }
+            // }
 
             // TODO: this is expensive since we're constantly spilling here...
             // a better approach would be to make Control Messages and Network Messages implement a generic
             // Message trait with isControlMessage and isNetworkMessage functions, and then use those
             // predicates to hand the message off to the right handler
             // .... currently, Control Messages are just strings
-            let control_attempt = self.control_channel.try_recv();
-            match control_attempt {
-                Ok(msg) => {
-                    println!("Processing a message!");
-                    self.process_control(msg);
-                },
-                Err(e) => {
-                    // println!("Error receiving a control message");
-                }
-            }
+
+            // let control_attempt = self.control_channel.try_recv();
+            // match control_attempt {
+            //     Ok(msg) => {
+            //         println!("Processing a message!");
+            //         self.process_control(msg);
+            //     },
+            //     Err(e) => {
+            //         // println!("Error receiving a control message");
+            //     }
+            // }
         }
     }
 
@@ -82,7 +87,7 @@ impl<'a> Processor<'a> {
                     // self.link_manager.add_link()
                 } else {
                     println!("Adding the link");
-                    self.link_manager.add_link(nick.to_owned(), LinkType::TCP, hostport.to_owned());
+                    // self.link_manager.add_link(nick.to_owned(), LinkType::TCP, hostport.to_owned());
                 }
             } else if target == "route" {
                 let mut name = params[2];
@@ -103,15 +108,15 @@ impl<'a> Processor<'a> {
             Ok((ForwarderResult::CacheHit, msg, id)) => { // content, return it
                 // forward the message to the ID
                 let inner_msg = msg.unwrap();
-                let result = self.output_queue.send((inner_msg, id));
-                match result {
-                    Ok(m) => {
-                        println!("Sent content object back to the link.");
-                    },
-                    Err(e) => {
-                        println!("Error: unable to send message to the link.");
-                    }
-                }
+                // let result = self.output_queue.send((inner_msg, id));
+                // match result {
+                //     Ok(m) => {
+                //         println!("Sent content object back to the link.");
+                //     },
+                //     Err(e) => {
+                //         println!("Error: unable to send message to the link.");
+                //     }
+                // }
             },
             Ok((ForwarderResult::PitHit, _, _)) => {
                 // do nothing, this is okay.
@@ -119,15 +124,15 @@ impl<'a> Processor<'a> {
             Ok ((ForwarderResult::ForwardInterest, msg, id)) => { // interest, forward it
                 // forward the message to the ID
                 let inner_msg = msg.unwrap();
-                let result = self.output_queue.send((inner_msg, id));
-                match result {
-                    Ok(m) => {
-                        println!("Sent content object back to the link.");
-                    },
-                    Err(e) => {
-                        println!("Error: unable to send message to the link.");
-                    }
-                }
+                // let result = self.output_queue.send((inner_msg, id));
+                // match result {
+                //     Ok(m) => {
+                //         println!("Sent content object back to the link.");
+                //     },
+                //     Err(e) => {
+                //         println!("Error: unable to send message to the link.");
+                //     }
+                // }
             },
             Err(e) => {
                 println!("Error in the forwarer -- check it out.");
@@ -140,17 +145,15 @@ impl<'a> Processor<'a> {
         // TODO
     }
 
-    pub fn process_message(&mut self, msg: Message, incomingFace: u16) {
-        // TODO: (1) CS, (2) PIT, (3) FIB
-        // if (self.fwd.cs.lookup(msg.))
-        println!("here we go!");
-
+    pub fn process_message(&mut self, msg: Message, incomingFace: u16)  {
+        // TODO: wrap these up in state checker functions
         if msg.packet_type == core::packet::message::PacketType::Interest {
             self.process_interest(msg, incomingFace);
         } else if msg.packet_type == core::packet::message::PacketType::ContentObject {
             self.process_content(msg, incomingFace);
         } else {
             // TODO: interest return
+            // return Err(UnsupportedMessageType)
         }
     }
 }
--- iris/src/main.rs
+++ iris/src/main.rs
@@ -1,3 +1,7 @@
+extern crate mio;
+
+use mio::*; // http://nbaksalyar.github.io/2015/07/10/writing-chat-in-rust.html
+
 mod common;
 mod control;
 mod core;
@@ -13,58 +17,61 @@ use std::error::Error;
 use std::io::prelude::*;
 use std::fs::File;
 use std::path::Path;
-use std::sync::mpsc::{Sender,Receiver};
-use std::sync::mpsc;
 
-use std::net::{SocketAddrV4, UdpSocket, IpAddr, Ipv4Addr};
+use std::collections::HashMap;
+use std::net::SocketAddr;
+use mio::tcp::*;
+use mio::udp::*;
 
 fn setup_listeners(control_sender: Sender<String>, sender: Sender<(core::packet::message::Message, u16)>) -> (Vec<Box<core::link::LinkListener>>, Vec<JoinHandle<()>>) {
     let mut listen_handles = Vec::new();
     let mut listeners = Vec::new();
 
     // Default control listener interface
-    let localhost = Ipv4Addr::new(127,0,0,1);
-    let default_ctl_addr = std::net::SocketAddrV4::new(localhost, 9698);
-    let default_ctl_listener = control::ControlListener::new(control_sender.clone(), default_ctl_addr);
-    let listen_result = default_ctl_listener.listen();
-    match listen_result {
-        Ok(listener) => {
-            println!("Created the control listener on 9698");
-            listen_handles.push(listener);
-        },
-        Err(e) => {
-            println!("Could not listen on port 9698");
-        }
-    }
+    // let localhost = Ipv4Addr::new(127,0,0,1);
+    // let default_ctl_addr = std::net::SocketAddrV4::new(localhost, 9698);
+    // let default_ctl_listener = control::ControlListener::new(control_sender.clone(), default_ctl_addr);
+    // let listen_result = default_ctl_listener.listen();
+    // match listen_result {
+    //     Ok(listener) => {
+    //         println!("Created the control listener on 9698");
+    //         listen_handles.push(listener);
+    //     },
+    //     Err(e) => {
+    //         println!("Could not listen on port 9698");
+    //     }
+    // }
 
-    // Default UDP listener interface
-    let localhost = Ipv4Addr::new(127,0,0,1);
-    let default_udp_addr = std::net::SocketAddrV4::new(localhost, 9696);
-    let default_udp_listener = core::link::UDPLinkListener::new(sender.clone(), default_udp_addr);
-    let listen_result = default_udp_listener.listen();
-    match listen_result {
-        Ok(listener) => {
-            println!("Created the UDP listener on 9696");
-            listen_handles.push(listener);
-        },
-        Err(e) => {
-            println!("Could not listen on 9696");
-        }
-    }
+    // TODO: bring these back when done with the control setup
 
-    // Default TCP listener interface
-    let default_tcp_addr = std::net::SocketAddrV4::new(localhost, 9697);
-    let default_tcp_listener = core::link::TCPLinkListener::new(sender.clone(), default_tcp_addr);
-    let listen_result = default_tcp_listener.listen();
-    match listen_result {
-        Ok(listener) => {
-            println!("Created the TCP listener on 9697");
-            listen_handles.push(listener);
-        },
-        Err(e) => {
-            println!("Could not listen on 9697");
-        }
-    }
+    // // Default UDP listener interface
+    // let localhost = Ipv4Addr::new(127,0,0,1);
+    // let default_udp_addr = std::net::SocketAddrV4::new(localhost, 9696);
+    // let default_udp_listener = core::link::UDPLinkListener::new(sender.clone(), default_udp_addr);
+    // let listen_result = default_udp_listener.listen();
+    // match listen_result {
+    //     Ok(listener) => {
+    //         println!("Created the UDP listener on 9696");
+    //         listen_handles.push(listener);
+    //     },
+    //     Err(e) => {
+    //         println!("Could not listen on 9696");
+    //     }
+    // }
+    //
+    // // Default TCP listener interface
+    // let default_tcp_addr = std::net::SocketAddrV4::new(localhost, 9697);
+    // let default_tcp_listener = core::link::TCPLinkListener::new(sender.clone(), default_tcp_addr);
+    // let listen_result = default_tcp_listener.listen();
+    // match listen_result {
+    //     Ok(listener) => {
+    //         println!("Created the TCP listener on 9697");
+    //         listen_handles.push(listener);
+    //     },
+    //     Err(e) => {
+    //         println!("Could not listen on 9697");
+    //     }
+    // }
 
     return (listeners, listen_handles);
 }
@@ -73,6 +80,145 @@ fn run(listeners: Vec<JoinHandle<()>>) {
     println!("Joining on the listeners");
     for listener in listeners {
         listener.join();
+    }
+}
+
+struct TCPLink<'a> {
+    socket: TcpStream,
+    // processor: &'a mut core::processor::Processor<'a>,
+    fib: &'a mut HashMap<String, Vec<Token>>,
+    id: Token
+}
+
+impl<'a> TCPLink<'a> {
+    fn read(&mut self) {
+        loop {
+            let mut buf = [0; 4096];
+            match self.socket.try_read(&mut buf) {
+                Err(e) => {
+                    println!("Error while reading socket: {:?}", e);
+                    return
+                },
+                Ok(None) =>
+                    // Socket buffer has got no more bytes.
+                    break,
+                Ok(Some(len)) => {
+                    // TODO: decode the message and send it to the thing
+                    let msg = core::packet::decode_packet(&buf[0..len]);
+                    // self.processor.process_message(msg, self.id.as_usize() as u16);
+                    // TODO: get the result and do something with it!
+                }
+            }
+        }
+    }
+
+    fn write(&self, msg: core::packet::message::Message) {
+        println!("Attempting to write a message to this socket");
+        // self.socket.write(msg.bytes())
+    }
+
+    // fn new(token: Token, socket: TcpStream, processor: &'a mut core::processor::Processor<'a>) -> TCPLink<'a> {
+    fn new(token: Token, socket: TcpStream, fib: &'a mut HashMap<String, Vec<Token>>) -> TCPLink<'a> {
+        TCPLink {
+            id: token,
+            socket: socket,
+            // processor: processor
+            fib: fib
+        }
+    }
+}
+
+struct LinkManager<'a> {
+    processor: &'a mut core::processor::Processor<'a>,
+    links: HashMap<Token, TCPLink<'a>>, // TOOD: make this a generic type of Link
+    token_counter: usize,
+    tcp_socket: TcpListener,
+    udp_socket: UdpSocket,
+    ctl_socket: UdpSocket,
+
+    // TODO: ADD OTHER STUFF HERE
+    fib: HashMap<String, Vec<Token>>
+}
+
+const SERVER_TCP_TOKEN: Token = Token(0);
+const SERVER_UDP_TOKEN: Token = Token(1);
+const SERVER_CTL_TOKEN: Token = Token(2);
+
+impl<'a> LinkManager<'a> {
+    // TODO: invoke this later...
+    fn emit_message(&mut self, msg: core::packet::message::Message, token: Token) {
+        self.links.get(&token).unwrap().write(msg);
+    }
+
+    fn new(tcp_socket: TcpListener, udp_socket: UdpSocket, ctl_socket: UdpSocket,
+        processor: &'a mut core::processor::Processor<'a>) -> LinkManager {
+        LinkManager {
+            processor: processor,
+            links: HashMap::new(),
+            tcp_socket: tcp_socket,
+            udp_socket: udp_socket,
+            ctl_socket: ctl_socket,
+            token_counter: 3,
+            fib: HashMap::new()
+        }
+    }
+}
+
+impl<'a> Handler for LinkManager<'a> {
+    type Timeout = usize;
+    type Message = ();
+
+    fn ready(&mut self, event_loop: &mut EventLoop<LinkManager>, token: Token, events: EventSet) {
+        match token {
+            SERVER_TCP_TOKEN => {
+                let client_socket = match self.tcp_socket.accept() {
+                    Err(e) => {
+                        println!("Accept error: {}", e);
+                        return;
+                    },
+                    Ok(None) => unreachable!("Accept has returned 'None'"),
+                    Ok(Some((sock, addr))) => sock
+                };
+
+                println!("Spawning a new TCP link");
+
+                self.token_counter += 1;
+                let new_token = Token(self.token_counter);
+
+                // CAW: this is the problem.
+                // let processor = self.processor;
+                let link = TCPLink::new(new_token.clone(), client_socket, &mut self.fib);
+
+                // self.links.insert(new_token, link);
+                // event_loop.register(&self.links[&new_token].socket,
+                //                         new_token, EventSet::readable(),
+                //                         PollOpt::edge() | PollOpt::oneshot()).unwrap();
+            },
+            SERVER_UDP_TOKEN => {
+                let mut buf = [0; 4096]; // 4k MTU for UDP, by default
+                match self.udp_socket.recv_from(&mut buf) {
+                    Err(e) => {
+                        // TODO
+                    },
+                    Ok(None) => {
+                        unreachable!("Accept has returned 'None'");
+                    },
+                    Ok(Some((amt,src))) => {
+                        println!("Got a new connection message"); // FOR EACH PACKET THIS IS CALLED
+                        // TODO: do something with this...
+                    }
+                }
+            },
+            SERVER_CTL_TOKEN => {
+                // TODO
+            }
+            token => {
+                let mut link = self.links.get_mut(&token).unwrap();
+                link.read();
+                event_loop.reregister(&link.socket, token, EventSet::readable(),
+                                      PollOpt::edge() | PollOpt::oneshot()).unwrap();
+            }
+        }
     }
 }
 
@@ -87,23 +233,66 @@ fn main() {
     let mut fwd = core::Forwarder::new(&fcs, &mut fpit, ffib);
 
     // Create the send/receive channel
-    let (itx, irx): (Sender<(core::packet::message::Message, u16)>, Receiver<(core::packet::message::Message, u16)>) = mpsc::channel(); // for receiving messages
-    let (otx, orx): (Sender<(core::packet::message::Message, u16)>, Receiver<(core::packet::message::Message, u16)>) = mpsc::channel(); // for sending messages
-    let (ctrl_tx, ctrl_rx): (Sender<String>, Receiver<String>) = mpsc::channel();
+    // let (itx, irx): (Sender<(core::packet::message::Message, u16)>, Receiver<(core::packet::message::Message, u16)>) = mpsc::channel(); // for receiving messages
+    // let (otx, orx): (Sender<(core::packet::message::Message, u16)>, Receiver<(core::packet::message::Message, u16)>) = mpsc::channel(); // for sending messages
+    // let (ctrl_tx, ctrl_rx): (Sender<String>, Receiver<String>) = mpsc::channel();
 
-    let (listeners, handlers) = setup_listeners(ctrl_tx, itx.clone());
+    // let (listeners, handlers) = setup_listeners(ctrl_tx, itx.clone());
 
-    let link_table = core::link::LinkTable::new();
-    let link_manager = core::link::LinkManager::new(itx, orx, link_table, listeners);
+    let mut processor = core::processor::Processor::new(fwd);
 
-    // TODO: need to put the output receiver queue in the link table, tie the listeners up to the link table, and then implement the logic in the link table to send information to the links
+    let default_tcp_listener = "127.0.0.1:9696".parse::<SocketAddr>().unwrap();
+    let server_tcp_socket = TcpListener::bind(&default_tcp_listener).unwrap();
 
-    // Create the processor to handle the link <-> core message passing
-    // Move the receiver into the processor
-    let mut processor = core::processor::Processor::new(fwd, irx, otx, ctrl_rx, link_manager);
-    processor.run();
+    let default_udp_listener = "127.0.0.1:9697".parse::<SocketAddr>().unwrap();
+    let server_udp_socket = UdpSocket::bound(&default_udp_listener).unwrap();
 
-    run(handlers);
+    let default_ctl_listener = "127.0.0.1:9698".parse::<SocketAddr>().unwrap();
+    let server_ctl_socket = UdpSocket::bound(&default_ctl_listener).unwrap();
+
+    let mut event_loop = EventLoop::new().unwrap();
+    event_loop.register(&server_tcp_socket,
+        Token(0),
+        EventSet::readable(),
+        PollOpt::edge()).unwrap();
+
+    event_loop.register(&server_udp_socket,
+        Token(1),
+        EventSet::readable(),
+        PollOpt::edge()).unwrap();
+
+    event_loop.register(&server_ctl_socket,
+        Token(2),
+        EventSet::readable(),
+        PollOpt::edge()).unwrap();
+
+    // let mut handler = LinkManager {
+    //     // input_channel: itx,
+    //     // output_channel: orx,
+    //     processor: &mut processor,
+    //     token_counter: 3,        // Starting the token counter from 3 (0 1 2 are the tcp,udp,ctl tokens)
+    //     links: HashMap::new(),   // Creating an empty HashMap
+    //     tcp_socket: server_tcp_socket,    // Handling the ownership of the socket to the struct
+    //     udp_socket: server_udp_socket,
+    //     ctl_socket: server_ctl_socket
+    // };
+
+    let mut handler = LinkManager::new(server_tcp_socket, server_udp_socket, server_ctl_socket, &mut processor);
+
+    event_loop.run(&mut handler).unwrap();
+
+
+    // let link_table = core::link::LinkTable::new();
+    // let link_manager = core::link::LinkManager::new(itx, orx, link_table, listeners);
+    //
+    // // TODO: need to put the output receiver queue in the link table, tie the listeners up to the link table, and then implement the logic in the link table to send information to the links
+    //
+    // // Create the processor to handle the link <-> core message passing
+    // // Move the receiver into the processor
+    // let mut processor = core::processor::Processor::new(fwd, irx, otx, ctrl_rx, link_manager);
+    // processor.run();
+    //
+    // run(handlers);
 }
 
 
