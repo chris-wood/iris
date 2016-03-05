124e718f48210d29faca23f45aab06e84ed9c709
Christopher Wood <christopherwood07@gmail.com>
2016-03-05 08:20:45 -0800

    Message update

--- iris/src/core/packet/message.rs
+++ iris/src/core/packet/message.rs
@@ -1,4 +1,5 @@
 use std::string::String as String;
+use std::str;
 
 use common::name as name;
 use common::name::Name as Name;
@@ -65,6 +66,11 @@ pub struct Message {
     pub message_bytes: Vec<u8>,
     pub packet_type: PacketType,
     pub name_offset: usize,
+    pub name_segment_offsets: Vec<(usize, usize)>,
+    pub key_id_offset: usize,
+    pub key_id_length: usize,
+    pub content_id_offset: usize,
+    pub content_id_length: usize,
     pub name_length: usize,
     pub payload_offset: usize,
     pub payload_length: usize,
@@ -84,7 +90,12 @@ impl Message {
         return Message {
             message_bytes: byteVector,
             name_offset: 0,
+            name_segment_offsets: Vec::new(),
             name_length: 0,
+            key_id_offset: 0,
+            key_id_length: 0,
+            content_id_offset: 0,
+            content_id_length: 0,
             packet_type: new_packet_type,
             payload_offset: new_payload_offset,
             payload_length: new_payload_length,
@@ -94,26 +105,93 @@ impl Message {
         }
     }
 
-    pub fn get_name(&self) -> Name {
-        let name_bytes = &self.message_bytes[self.name_offset .. (self.name_offset + self.name_length)];
-        let mut byte_vector = Vec::new();
-        for b in name_bytes {
-            byte_vector.push(*b);
-        }
-
-        let name_string = match String::from_utf8(byte_vector) {
-            Ok(v) => v,
-            Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
-        };
-
-        let mut name_vector = Vec::new();
-        let splits:Vec<&str> = name_string.split('/').collect();
-        for component in splits {
-            name_vector.push(String::from(component));
-        }
-
-        return Name::new(name_vector);
+    pub fn bytes(self) -> Vec<u8> {
+        return self.message_bytes.clone()
     }
+
+    pub fn get_key_id_overlay(&self) -> Option<(usize, usize)> {
+        match (self.key_id_offset, self.key_id_length) {
+            (o, l) if o > 0 && l > 0 => Some((o, l)),
+            _ => None
+        }
+    }
+
+    pub fn get_content_id_overlay(&self) -> Option<(usize, usize)> {
+        match (self.content_id_offset, self.content_id_length) {
+            (o, l) if o > 0 && l > 0 => Some((o, l)),
+            _ => None
+        }
+    }
+
+    pub fn get_name(&self) -> Name {
+        let mut name_bytes = Vec::new();
+
+        let mut index = 0;
+        let length = self.name_length;
+        let offset = self.name_offset;
+
+        while index < length {
+            name_bytes.push(self.message_bytes[offset + index]);
+            index = index + 1;
+        }
+
+        // let num_components = self.name_segment_offsets.len();
+        let mut name_segment_offsets = Vec::new(); // =
+        let mut offsets = self.name_segment_offsets.clone(); //Vec::new();
+        for (o, l) in offsets {
+            name_segment_offsets.push((o - self.name_offset, l));
+        }
+
+        return Name::create_from_bytes(&name_bytes, &name_segment_offsets).unwrap();
+    }
+
+    // pub fn compare_name(&self, other_name: &Name) -> bool {
+    //     if other_name.len() != self.name_length {
+    //         return false;
+    //     }
+    //
+    //     let mut index = 0;
+    //     let mut length = other_name.len();
+    //
+    //     while index < length {
+    //         if other_name.at(index) != self.message_bytes[self.name_offset + index] {
+    //             return false;
+    //         }
+    //         index = index + 1;
+    //     }
+    //
+    //     return true;
+    // }
+
+    // pub fn get_name(&self) -> Name {
+    //     // let name_bytes = &self.message_bytes[self.name_offset .. (self.name_offset + self.name_length)];
+    //     // let mut byte_vector = Vec::new();
+    //     // for b in name_bytes {
+    //     //     byte_vector.push(*b);
+    //     // }
+    //     //
+    //     // let name_string = match String::from_utf8(byte_vector) {
+    //     //     Ok(v) => v,
+    //     //     Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+    //     // };
+    //
+    //     let mut name_vector = Vec::new();
+    //     for (offset, length) in self.name_segment_offsets {
+    //         let string = str::from_utf8(&self.message_bytes[offset .. (offset + length)]).unwrap();
+    //
+    //         // let mut full_string = String::new();
+    //         // full_string.push_str(string);
+    //
+    //         name_vector.push(full_string);
+    //     }
+    //
+    //     // let splits:Vec<&str> = name_string.split('/').collect();
+    //     // for component in splits {
+    //     //     name_vector.push(String::from(component));
+    //     // }
+    //
+    //     return Name::create_from_components(name_vector);
+    // }
 
     // TODO: isControl, isInterest, isContentObject
 
@@ -128,3 +206,19 @@ impl Message {
         println!("  validation_length = {}", self.validation_length);
     }
 }
+
+#[test]
+fn test_get_name() {
+
+}
+
+#[test]
+fn test_get_key_id() {
+
+}
+
+#[test]
+fn test_get_content_id() {
+
+}
+
