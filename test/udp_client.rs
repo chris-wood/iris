c5500c20b82b64ee48eed7923447a98018a5d38d
Christopher Wood <christopherwood07@gmail.com>
2016-03-15 06:20:15 -0700

    test UDP client

--- test/udp_client.rs
+++ test/udp_client.rs
@@ -3,7 +3,7 @@ use std::thread;
 
 fn main()
 {
-    let attempt = UdpSocket::bind("127.0.0.1:9695");
+    let attempt = UdpSocket::bind("127.0.0.1:9700");
     let mut socket;
     match attempt {
         Ok(sock) => {
@@ -11,23 +11,25 @@ fn main()
             socket = sock;
         },
         Err(err) => {
-            panic!("Unable to bind to 127.0.0.1:9695");
+            panic!("Unable to bind to 127.0.0.1:9700");
         }
     }
 
     //let mut buf = [0; 10];
     //let (amt, src) = socket.recv_from(&mut buf).unwrap();
 
+    let cmd = "mk link firstLink tcp 127.0.0.1:9699".as_bytes();
+
     let raw = [1; 10];
     let buf = &raw[..10];
 
-    let result = socket.send_to(buf, "localhost:9696");
+    let result = socket.send_to(&cmd, "127.0.0.1:9698");
     match result {
         Ok(bytes) => {
             println!("Sent {} bytes", bytes);
         },
         Err(err) => {
-            panic!("Failed to send to localhost:9999");
+            panic!("Failed to send to 127.0.0.1:9698");
         }
     }
 
